5be3a8817a2b0457cedbe70590e76cfa729edaaa
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-03-08 09:37:47 -0300

    agregando project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -10,6 +10,7 @@ import workWheatherAppJs from "../../assets/workWheatherAppJs.jpg";
 import workBookCreate from "../../assets/workBookCreate.jpg";
 import workLoginAdmin from "../../assets/workLoginAdmin.jpg";
 import workCrudReact from "../../assets/workCrudReact.jpg";
+import workAdvancedCart from "../../assets/workCart.jpg";
 
 export const projectsData = [
   {
@@ -21,43 +22,49 @@ export const projectsData = [
   },
   {
     id: 2,
+    image: workAdvancedCart,
+    title: "Shopping cart",
+    category: "app",
+    link: "https://advanced-shopping-cart-reactjs.netlify.app",
+  },
+  {
+    id: 3,
     image: workCoinApi,
     title: "Crypto price API",
     category: "app",
     link: "https://coingeckoapi-reactjs.netlify.app",
   },
   {
-    id: 3,
+    id: 4,
     image: workEcommerceReact,
     title: "E-commerce",
     category: "app",
     link: "https://e-commerce-quiroga-lucas.netlify.app",
   },
   {
-    id: 2,
+    id: 5,
     image: workBookCreate,
     title: "Bookstore",
     category: "app",
     link: "https://creation-books.netlify.app/",
   },
-
   ,
   {
-    id: 5,
+    id: 6,
     image: workLoginAdmin,
     title: "Login admin",
     category: "app",
     link: "https://login-admin-app.netlify.app/",
   },
   {
-    id: 6,
+    id: 7,
     image: workCrudReact,
     title: "CRUD",
     category: "app",
     link: "https://crudbyreact.netlify.app/",
   },
   {
-    id: 7,
+    id: 8,
     image: workWheatherAppJs,
     title: "Weather API",
     category: "app",
